f48cabe9a06687176131db43d6b4fb60126cd3e4
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-03-22 21:16:06 -0300

    feat: Adicionando novas rotas para a comparação de imagens com e sem lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LazyImagem from '../views/LazyImagemView.vue'
+import Imagem from '../views/ImageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/lazyimagem',
+      name: 'LazyImagem',
+      component: LazyImagem
+    },
+    {
+      path: '/imagem',
+      name: 'Imagem',
+      component: Imagem
     },
   ]
 })
